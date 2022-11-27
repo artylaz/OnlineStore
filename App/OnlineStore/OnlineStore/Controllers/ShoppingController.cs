89e5caf8b1a5a4fb326cc0a4dbbdabb9326bc212
Arty <arty-laz@mail.ru>
2022-11-28 01:48:30 +0500

    Добавил функционал вывода продуктов

--- App/OnlineStore/OnlineStore/Controllers/ShoppingController.cs
+++ App/OnlineStore/OnlineStore/Controllers/ShoppingController.cs
@@ -2,21 +2,48 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using OnlineStore.Data;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models.ViewModels;
 
 namespace OnlineStore.Controllers
 {
     public class ShoppingController: Controller
     {
-        [HttpGet]
-        public IActionResult ShowProducts(int idCategory)
+        private readonly OnlineStore_DbContext db;
+        public ShoppingController(OnlineStore_DbContext db)
         {
-            
+            this.db = db;
         }
 
         [HttpGet]
-        public IActionResult ShowProduct(int idProduct)
+        public IActionResult ShowProducts(Category category)
         {
-            
+            List<Product> products = new List<Product>();
+
+            if (category.CategoryId == null)
+            {
+                var categories = db.Categories.Where(c => c.CategoryId == category.Id).ToList();
+
+                foreach (var item in categories)
+                {
+                    products.AddRange(db.Products.Where(p => p.CategoryId == item.Id).Include(p => p.Pictures));
+                }
+            }
+            else
+            {
+                products = db.Products.Where(p => p.CategoryId == category.Id).Include(p=>p.Pictures).ToList();
+            }
+
+            var showProductsVM = new ShowProductsVM { Products = products, Category = category };
+
+            return View(showProductsVM);
+        }
+
+        [HttpGet]
+        public IActionResult ShowProduct(Product product)
+        {
+            return View();
         }
     }
 }
---
+++ App/OnlineStore/OnlineStore/Models/ViewModels/ShowProductsVM.cs
@@ -0,0 +1,15 @@
+﻿using OnlineStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models.ViewModels
+{
+    public class ShowProductsVM
+    {
+        public List<Product> Products { get; set; }
+        public Category Category { get; set; }
+    }
+}
